4670a1c88b9a29532c9625c6587d5a9464b02176
Husanali Turdaliyev <Husanali Turdaliyev>
2026-02-11 17:38:22 +0500

    first commit

--- main/views.py
+++ main/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import *
+
+def hello_view(request):
+    return HttpResponse(
+    """    
+    <H1>Hello World!</H1>
+    <hr>
+    <p>from view</p>
+    """
+    )
+
+
+def home_view(request):
+    return render(request, "home.html")
+
+def students_view(request):
+    talabalar = Talaba.objects.all()
+    data = {
+        'talabalar': talabalar,
+    }
+    return render(request, "students.html", context=data)
+
+def student_details_view(request, student_id):
+    talaba = Talaba.objects.get(id=student_id)
+    data = {
+        'talaba': talaba,
+    }
+    return render(request, "student.html", context=data)
+
+
+def mualliflar_view(request):
+    mualliflar = Muallif.objects.all()
+    contex = {
+        'mualliflar' : mualliflar,
+    }
+    return render(request, "mualliflar.html", context=contex)
+
+def muallif_view(request, muallif_id):
+    muallif = Muallif.objects.get(id=muallif_id)
+    contex = {
+        'muallif': muallif,
+    }
+    return render(request, "muallif.html", context=contex)
